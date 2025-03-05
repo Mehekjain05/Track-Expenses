94b275a7eeefb827e97d1c9fb843dfc9f6631da7
Mehek Jain <mehekjain28@gmail.com>
2025-03-05 20:25:42 +0530

    user input and error handling

--- data_entry.py
+++ data_entry.py
@@ -1,5 +1,8 @@
 from datetime import datetime
-
+CATEGORIES = {
+    'I' : "Incoming",
+    'O' : "Outgoing"
+}
 
 def get_date(prompt, allow_default = False):
     date_str = input(prompt)
@@ -14,10 +17,29 @@ def get_date(prompt, allow_default = False):
         return get_date(prompt, allow_default)
 
 def get_amount():
-    pass
+    try:
+        amt = input("Enter the amount")
+        if amt <= 0:
+            print("Amount should be positive and Nonzero")
+        return amt    
+    except ValueError as e:
+        print("Please enter a valid amount.." + e)
+        return get_amount()
+                
 
 def get_category():
-    pass
+    try:
+        category = input("Entry I => Credited or O => Debited").upper()
+        for category in CATEGORIES:
+            return CATEGORIES[category]
+    except ValueError as e:
+        print("Enter category from the options" + e)
+        return get_category()    
 
 def get_description():
-    pass
+    try:
+        description = input("Enter the description")
+        return description
+    except Exception as e:
+        print("Enter a valid Description" + e)
+        return get_description()
