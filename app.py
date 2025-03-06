9f73f63fb72bc1f619eb1dcdbe246ed33944259b
Mehek Jain <mehekjain28@gmail.com>
2025-03-06 23:10:37 +0530

    ready to test

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import csv
 from datetime import datetime
-
+from data_entry import get_amount, get_category, get_date, get_description
 class Finance_Tracker:
     CSV_file = "budget_tracker.csv"
     COLUMNS = ["date", "amount", "category", "description"]
@@ -27,10 +27,17 @@ class Finance_Tracker:
             writer = csv.DictWriter(file, fieldnames=cls.COLUMNS)
             writer.writerow(addItem)
         print("Entry Added Successfully")    
+        
+    def main(cls):
+        Finance_Tracker.intialize_csv()
+        date = get_date("Enter the date of the transaction dd-mm-yy", allow_default=True)
+        amount = get_amount()
+        category = get_category()
+        description = get_description()
+        Finance_Tracker.append_to_csv(date=date, amount=amount, category=category, description= description)          
             
             
-            
-    
+    main()
     
     
 # Finance_Tracker.append_to_csv("27-02-2025", 500.00, "Incoming", "PocketMoney")                
